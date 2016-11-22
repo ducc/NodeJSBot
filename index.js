85955555511cd2a6ee6a3650c6c4e6577582f7a3
SpongyBacon <spongybacon@hotmail.co.uk>
2016-11-22 13:46:45 +0000

    Use event constants

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ client.connect({
     token: config.token
 });
 
-client.Dispatcher.on("GATEWAY_READY", function() {
+//noinspection JSUnresolvedVariable
+client.Dispatcher.on(Discordie.Events.GATEWAY_READY, function() {
     console.log("Connected as: " + client.User.username)
 });
 
-client.Dispatcher.on("MESSAGE_CREATE", function(e) {
+client.Dispatcher.on(Discordie.Events.MESSAGE_CREATE, function(e) {
     var msg = Util.format(MESSAGE_FORMAT, e.message.guild.name, e.message.channel.name, e.message.author.username,
         e.message.content);
     console.log(msg);
