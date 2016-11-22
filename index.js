d74afe6794d044eb48e76d88529f686d2a4d0755
SpongyBacon <spongybacon@hotmail.co.uk>
2016-11-22 13:09:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var Util = require("util");
+var Discordie = require("discordie");
+var YamlConfig = require("node-yaml-config");
+
+const MESSAGE_FORMAT = "[%s - #%s] %s: %s";
+
+var client = new Discordie();
+var config = YamlConfig.load(__dirname + "/config.yml");
+
+client.connect({
+    token: config.token
+});
+
+client.Dispatcher.on("GATEWAY_READY", function() {
+    console.log("Connected as: " + client.User.username)
+});
+
+client.Dispatcher.on("MESSAGE_CREATE", function(e) {
+    var msg = Util.format(MESSAGE_FORMAT, e.message.guild.name, e.message.channel.name, e.message.author.username,
+        e.message.content);
+    console.log(msg);
+    if (e.message.content == "(╯°□°）╯︵ ┻━┻") {
+        e.message.channel.sendMessage("FUQQ >!\"<\"EL\"RJ\"2qf2");
+    }
+});
