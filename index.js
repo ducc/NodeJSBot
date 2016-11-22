44f832cb05827926a43453b00d883835d75206fb
SpongyBacon <spongybacon@hotmail.co.uk>
2016-11-22 15:31:42 +0000

    selfbotto

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-var Util = require("util");
-var Discordie = require("discordie");
-var YamlConfig = require("node-yaml-config");
-
+const Util = require("util");
+const Discordie = require("discordie");
+const YamlConfig = require("node-yaml-config");
 const MESSAGE_FORMAT = "[%s - #%s] %s: %s";
+const client = new Discordie();
+const config = YamlConfig.load(__dirname + "/config.yml");
 
-var client = new Discordie();
-var config = YamlConfig.load(__dirname + "/config.yml");
+let prefix = "s";
+let userId;
+let ready = false;
+
+/*
+because with the prefix it makes spong.ping()
+ */
+class Pong {
+    constructor() {
+    }
+
+    help() {
+        return "get some pls";
+    }
+
+    ping() {
+        return "<@" + userId + ">";
+    }
+
+    clean(channel, num) {
+        const messages = channel.messages.reverse();
+        if (num == -1) num = 1000;
+        let count = 0;
+        for (let i = 0; i < messages.length; i++) {
+            let message = messages[i];
+            if (message.deleted) continue;
+            if (message.author.id !== userId) continue;
+            count++;
+            message.delete();
+            if (count > num) {
+                break;
+            }
+        }
+        return "ok cleaning " + channel.name;
+    }
+}
+
+const pong = new Pong();
 
 client.connect({
     token: config.token
@@ -14,13 +51,39 @@ client.connect({
 //noinspection JSUnresolvedVariable
 client.Dispatcher.on(Discordie.Events.GATEWAY_READY, function() {
     console.log("Connected as: " + client.User.username)
+    userId = client.User.id;
+    ready = true;
 });
 
 client.Dispatcher.on(Discordie.Events.MESSAGE_CREATE, function(e) {
-    var msg = Util.format(MESSAGE_FORMAT, e.message.guild.name, e.message.channel.name, e.message.author.username,
+    if (!ready) return;
+    const msg = Util.format(MESSAGE_FORMAT, e.message.guild.name, e.message.channel.name, e.message.author.username,
         e.message.content);
     console.log(msg);
-    if (e.message.content == "(╯°□°）╯︵ ┻━┻") {
-        e.message.channel.sendMessage("┬─┬ ノ( ゜-゜ノ)");
+    if (e.message.author.id !== userId || !e.message.content.startsWith(prefix)) return;
+    let content = e.message.content.substring(1);
+    let newMsg = false;
+    if (content.startsWith("#")) {
+        content = content.substring(1);
+        newMsg = true;
+    }
+    let channel = e.message.channel;
+    try {
+        let result = eval(content);
+        console.log(result);
+        if (result !== undefined) {
+            if (newMsg) {
+                channel.sendMessage(result);
+            } else {
+                e.message.edit(e.message.content.substring(1) + ": " + result);
+            }
+        }
+    } catch (ex) {
+        console.log(ex);
+        if (newMsg) {
+            channel.sendMessage(ex.message);
+        } else {
+            e.message.edit(e.message.content.substring(1) + ": " + ex.message);
+        }
     }
 });
